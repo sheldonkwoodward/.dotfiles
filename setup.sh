ca63337caa25de0fb95bd3024c84db0f4093e307
sheldonwoodward <sheldon.k.woodward@gmail.com>
2018-08-27 12:45:00 -0400

    More files.

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# get home directory
+CD="$(pwd)"
+
+# hard link files
+ln $CD/.bash_profile $HOME/.bash_profile
+ln $CD/.gitignore_global $HOME/.gitignore_global
+ln $CD/.gitconfig $HOME/.gitconfig
+
+# set better git cli colors
+git config --global color.ui true
+git config --global color.diff-highlight.oldNormal    "red bold"
+git config --global color.diff-highlight.oldHighlight "red bold 52"
+git config --global color.diff-highlight.newNormal    "green bold"
+git config --global color.diff-highlight.newHighlight "green bold 22"
+git config --global color.diff.meta       "yellow"
+git config --global color.diff.frag       "magenta bold"
+git config --global color.diff.commit     "yellow bold"
+git config --global color.diff.old        "red bold"
+git config --global color.diff.new        "green bold"
+git config --global color.diff.whitespace "red reverse"
