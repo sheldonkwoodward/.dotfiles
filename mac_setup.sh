9569cbf5763590cfa1fd4214edfa1c42dcfd01d3
Sheldon Woodward <sheldon.woodward@ahss.org>
2018-08-27 12:01:04 -0400

    Added git to brew dependencies.

--- mac_setup.sh
+++ mac_setup.sh
@@ -2,6 +2,6 @@
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update && brew upgrade
-brew install bat diff-so-fancy htop micro node pipenv prettyping python2 python3 trash tree vim
+brew install bat diff-so-fancy git htop micro node pipenv prettyping python2 python3 trash tree vim
 
 # link python3
