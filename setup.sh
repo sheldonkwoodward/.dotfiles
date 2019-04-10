46ac525f9dd6ef1cffd6b16ae4f16c8f31fd394e
sheldonkwoodward <sheldon.k.woodward@gmail.com>
2019-04-09 22:22:07 -0700

    Added .pylintrc and updated setup.sh to link .editorconfig and .pylintrc.

--- setup.sh
+++ setup.sh
@@ -4,13 +4,14 @@
 CD="$(pwd)"
 
 # hard link files
-ln -f $CD/.bash_profile $HOME/.bash_profile
-ln -f $CD/.editorconfig $HOME/.editorconfig
-ln -f $CD/.git_completion.sh $HOME/.git_completion.sh
-ln -f $CD/.gitconfig $HOME/.gitconfig
-ln -f $CD/.gitignore $HOME/.gitignore
-ln -f $CD/.pypirc $HOME/.pypirc
-ln -f $CD/.vimrc $HOME/.vimrc
+ln -f .bash_profile $HOME/.bash_profile
+ln -f .editorconfig $HOME/.editorconfig
+ln -f .git_completion.sh $HOME/.git_completion.sh
+ln -f .gitconfig $HOME/.gitconfig
+ln -f .gitignore $HOME/.gitignore
+ln -f .pypirc $HOME/.pypirc
+ln -f .pylintrc $HOME/.pylintrc
+ln -f .vimrc $HOME/.vimrc
 
 # create ~/.profile
 touch ~/.profile
